05e56e3859dcb43894f7ca490e96b21b91851141
khyleB <khyle.best@outlook.com>
2024-12-05 04:20:00 -0500

    Index redesign

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#091C2B";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
+
+
+
 let blob = document.querySelectorAll('.text-blob-container');
 
 window.addEventListener('DOMContentLoaded', ( ) => {
